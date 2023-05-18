efb997a061661c4067a92548da5728ff03772a36
pujaKundu <iampujakundu@gmail.com>
2023-05-18 17:46:17 +0600

    settings

--- app/index.js
+++ app/index.js
@@ -1,9 +1,29 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+const bgStyle = { flex: 1, backgroundColor: COLORS.lightWhite };
 
 const Home = () => {
-    return <View>
-      <Text>Home</Text>
-  </View>;
+  const router = useRouter();
+  return (
+    <SafeAreaView style={bgStyle}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+        }}
+      />
+    </SafeAreaView>
+  );
 };
 
 export default Home;
