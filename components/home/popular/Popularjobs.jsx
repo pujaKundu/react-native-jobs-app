c371e1cb43ce542ff6edfd0baa97c28dcc50fea0
pujaKundu <iampujakundu@gmail.com>
2023-05-20 18:38:18 +0600

    working on data fetching

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -12,10 +12,13 @@ import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
+import useFetch from '../../../hook/useFetch'
+
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+  const { data,isLoading,error} = useFetch('search', {
+    query:'React eveloper',num_pages:1
+  })
   return (
     <View style={styles.container}>
       <View style={styles.header}>
